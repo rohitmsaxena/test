16b4a7f4396734d2e26fc79d9891cc33529613c7
rohitsaxena <1125040@iheartmedia.com>
2022-05-29 17:29:20 -0600

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {AppBar, Box, Button, IconButton, Modal, Toolbar, Typography} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import Login from "./components/Login/Login.lazy";
 
 function App() {
-  return (
+    const [open, setOpen] = React.useState(false);
+    const handleLoginOpen = () => setOpen(true);
+    const handleLoginClose = () => setOpen(false);
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                        >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                        Awesome Library of Books and Things
+                    </Typography>
+                    <Button color="inherit" onClick={handleLoginOpen}>Login</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+        <Modal open={open} onClose={handleLoginClose}>
+            <Login />
+        </Modal>
     </div>
   );
 }
